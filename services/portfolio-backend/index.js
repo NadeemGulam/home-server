94a9ab3eecb16023b54582a2d710e812b52acad0
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2026-02-14 17:53:47 +0530

    fix: route path issue

--- services/portfolio-backend/index.js
+++ services/portfolio-backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8002;
 
 // Import routes
-const skillsRouter = require('./routes/skills');
+const skillsRouter = require('./src/routes/skills');
 
 // Use routes
 app.use('/skills', skillsRouter);
