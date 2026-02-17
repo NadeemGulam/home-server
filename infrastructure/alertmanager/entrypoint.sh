840e5d5b55b13d20cb60bb28912168a02ae6324d
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2026-02-18 01:47:07 +0530

    fix: use sed for alertmanager env var substitution

--- infrastructure/alertmanager/entrypoint.sh
+++ infrastructure/alertmanager/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# Substitute environment variables in the config template
-envsubst < /etc/alertmanager/config.yml.template > /etc/alertmanager/config.yml
+# Substitute environment variables in the config template using sed
+sed -e "s|\${GMAIL_APP_PASSWORD}|${GMAIL_APP_PASSWORD}|g" \
+    -e "s|\${GMAIL_FROM}|${GMAIL_FROM}|g" \
+    -e "s|\${ALERT_EMAIL_TO}|${ALERT_EMAIL_TO}|g" \
+    /etc/alertmanager/config.yml.template > /tmp/config.yml
 
 # Start Alertmanager with the processed config
-exec /bin/alertmanager "$@"
+exec /bin/alertmanager --config.file=/tmp/config.yml "$@"
